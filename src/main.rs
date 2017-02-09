13eb126eefe0e728c767d09a1b715515f154daa1
Givi Khojanashvili <gigovich@gmail.com>
2017-02-10 00:07:43 +0300

    Let's rock!

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,43 @@
 extern crate gtk;
 
-use gtk::prelude::*;
-use gtk::{Button, Window, WindowType};
+#[cfg(feature = "gtk_3_10")]
+mod main {
+    use gtk::prelude::*;
+    use gtk::{Button, Window, WindowType};
 
-fn main() {
-    if gtk::init().is_err() {
-        println!("Failed to initialize gtk!");
-        return;
+    fn main() {
+        if gtk::init().is_err() {
+            println!("Failed to initialize gtk!");
+            return;
+        }
+
+        let window = Window::new(WindowType::Toplevel);
+        window.set_title("First GTK program");
+        window.set_default_size(350, 70);
+
+        let button = Button::new_with_label("Click me!");
+        window.add(&button);
+        window.show_all();
+
+        window.connect_delete_event(|_, _| {
+            gtk::main_quit();
+            Inhibit(false)
+        });
+
+        button.connect_clicked(|_| {
+            println!("Clicked");
+        });
+
+        gtk::main();
     }
+}
 
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("First GTK program");
-    window.set_default_size(350, 70);
-    let button = Button::new_with_label("Click me!");
-    window.add(&button);
-    window.show_all();
+#[cfg(feature = "gtk_3_10")]
+fn main() {
+    prog::main()
+}
 
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    button.connect_clicked(|_| {
-        println!("Clicked");
-    });
-
-    gtk::main();
+#[cfg(not(feature = "gtk_3_10"))]
+fn main() {
+    println!("This version of GTK not supported");
 }
