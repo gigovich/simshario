7d19071cb7abf7011bb6ed0cd32c58f247e5c651
Givi Khojanashvili <gigovich@gmail.com>
2017-02-10 19:37:32 +0300

    Got compile state.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,29 @@
 extern crate gtk;
 
 #[cfg(feature = "gtk_3_10")]
-mod main {
+mod app {
+    use gtk;
     use gtk::prelude::*;
-    use gtk::{Button, Window, WindowType};
+    use gtk::{Builder, Window};
 
-    fn main() {
+    pub fn main() {
         if gtk::init().is_err() {
             println!("Failed to initialize gtk!");
             return;
         }
 
-        let window = Window::new(WindowType::Toplevel);
-        window.set_title("First GTK program");
-        window.set_default_size(350, 70);
+        let glide_src = include_str!("./gui/main.ui");
+        let builder = Builder::new_from_string(glide_src);
+        let window: Window = builder.get_object("mainWindow").unwrap();
 
-        let button = Button::new_with_label("Click me!");
-        window.add(&button);
         window.show_all();
-
-        window.connect_delete_event(|_, _| {
-            gtk::main_quit();
-            Inhibit(false)
-        });
-
-        button.connect_clicked(|_| {
-            println!("Clicked");
-        });
-
         gtk::main();
     }
 }
 
 #[cfg(feature = "gtk_3_10")]
 fn main() {
-    prog::main()
+    app::main()
 }
 
 #[cfg(not(feature = "gtk_3_10"))]
