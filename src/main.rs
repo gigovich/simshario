f2fbeb687cc923a6f7bd1b93ee091fac82fb1184
Givi Khojanashvili <gigovich@gmail.com>
2017-02-22 19:04:29 +0300

    Broken.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
+#[macro_use]
+extern crate log;
 extern crate gtk;
 
 #[cfg(feature = "gtk_3_10")]
 mod app {
-    use gtk;
+    use std::io;
+    use std::error::Error;
     use gtk::prelude::*;
     use gtk::{Builder, Window};
+    use gtk;
+    use std::io::prelude::{Read};
+    use std::net::{TcpListener, TcpStream, Shutdown};
+
+    fn process(s: TcpStream) -> Result<(), Box<Error>> {
+        let mut stream = s;
+        let mut buf = Vec::new();
+
+        if let Err(err) = stream.read_to_end(&mut buf) {
+            error!("read from connection {}", err);
+            stream.shutdown(Shutdown::Both).unwrap();
+        }
+        Ok(())
+    }
+
+    fn listen() {
+        let listener = TcpListener::bind("127.0.0.1:4812").unwrap();
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    if let Err(err) = process(stream) {
+                        error!("process connection {}", err);
+                    }
+                }
+                Err(e) => {
+                    error!("accept connection {}", e);
+                }
+            }
+        }
+    }
 
     pub fn main() {
         if gtk::init().is_err() {
@@ -16,7 +50,17 @@ mod app {
         let builder = Builder::new_from_string(glide_src);
         let window: Window = builder.get_object("mainWindow").unwrap();
 
+        window.connect_delete_event(|_, _| {
+            gtk::main_quit();
+            Inhibit(false)
+        });
+
+        window.connect_destroy(|_| {
+            println!(">>>>>");
+        });
+
         window.show_all();
+        listen();
         gtk::main();
     }
 }
